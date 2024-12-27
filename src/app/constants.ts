2ae025b964bc97e28cd46d344afa058e4688e203
Nagendra M <nagendram@propelapps.com>
2024-12-27 10:05:17 +0530

    Propel Business Config Dashboard removed redirects and changed the base url for configs

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,7 @@
 
-const baseUrl = 'http://10.168.194.55:3000/';
+// const baseUrl = 'http://10.168.194.55:3000/';
+const baseUrl = 'https://testnode.propelapps.com/';
+
 
 export const api_url = {
     updateBusinessConfig: `${baseUrl}EBS/24D/businessConfig`,
