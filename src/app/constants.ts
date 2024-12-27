08a8e9801716d588af2f00f05467829fd5bd7544
Navyasri Vuppalavanchu <navyasriv@propelapps.com>
2024-12-27 15:55:38 +0530

    baseurl updated

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,8 +1,8 @@
 
-// const baseUrl = 'http://10.168.194.55:3000/';
-const baseUrl = 'https://testnode.propelapps.com/';
+const baseUrl = 'http://10.168.194.55:3000/';
+const baseGetUrl='https://testnode.propelapps.com/'
 
 export const api_url = {
     updateBusinessConfig: `${baseUrl}EBS/24D/businessConfig`,
-    getBusinessConfig: `${baseUrl}getBusinessConfig/`
+    getBusinessConfig: `${baseGetUrl}getBusinessConfig/`
 }
